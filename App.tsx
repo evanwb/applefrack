857247ae6e8eb5da028eae05a508ee2e841a3184
evanwb <evanwburton@gmail.com>
2023-03-24 23:15:25 -0400

    jobs page

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Signup from "./Signup";
+import Jobs from "./Jobs";
 
 export default function App() {
-  return <Signup />;
+  return <Jobs />;
 }
 
 const styles = StyleSheet.create({
